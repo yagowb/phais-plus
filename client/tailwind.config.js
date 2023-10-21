9d6631ba8d8c46fae5e75b8750d566226056709c
Victor Kauan <victorkauan.dev@gmail.com>
2023-10-21 12:41:31 -0300

    :bug: fix: Correção das configurações de telas e cores personalizadas no Tailwind CSS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,55 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      xs: '400px',
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      bg: {
-          main: '#2C3A3B',
-          sec: '#476A6F',
-          layer: '#364546',
-          'layer-hover': '#425657'
-      },
-      green: {
-        main: '#7EB09B',
-        light: '#B0E2D5',
-        dark: '#64907E',
-        transp: '#AADBCE'
-      },
-      others: {
-        red: '#7EB09B',
-        yellow: '#B0E2D5',
-        blue: '#64907E',
-        green: '#64907E',
-      },
-      grays: {
-        disabled: '#414F50',
-        'other-drugs': '#485058',
-        component: '#CCCCCC',
-      },
-      white: {
-        100: "#F4F4F5",
-        200: "#E2E4E8",
-        300: "#A1A1AA",
-      }
-    },
     extend: {
+      screens: {
+        xs: "400px",
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        bg: {
+          main: "#2C3A3B",
+          sec: "#476A6F",
+          layer: "#364546",
+          "layer-hover": "#425657",
+        },
+        green: {
+          main: "#7EB09B",
+          light: "#B0E2D5",
+          dark: "#64907E",
+          transp: "#AADBCE",
+        },
+        others: {
+          red: "#7EB09B",
+          yellow: "#B0E2D5",
+          blue: "#64907E",
+          green: "#64907E",
+        },
+        grays: {
+          disabled: "#414F50",
+          "other-drugs": "#485058",
+          component: "#CCCCCC",
+        },
+        white: {
+          100: "#F4F4F5",
+          200: "#E2E4E8",
+          300: "#A1A1AA",
+        },
+      },
       borderRadius: {
-        '4xl': '2rem',
-      }
+        "4xl": "2rem",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
