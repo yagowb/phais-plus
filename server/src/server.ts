640cce151e276e42557a20edeceae29523a50f8c
yagowb <yagodev3@gmail.com>
2023-10-29 18:15:50 -0300

    init

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
-app.use("/registers", registerRouter);
+app.use("/registers", registerRouter); //aaaaa
 app.use("/medications", medicationRouter);
 app.use("/auth", authRouter);
 
