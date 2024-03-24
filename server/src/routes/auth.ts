f50caec01802f446230331505c8f6d730c88727b
Victor Kauan <victorkauan.dev@gmail.com>
2024-03-24 20:37:34 -0300

    :sparkles: feat: Create user microservice

--- server/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { AuthController } from "../controllers/AuthController";
-
-const router = Router();
-const authController = new AuthController();
-
-router.post("/authenticate", authController.authenticate);
-
-export { router };
