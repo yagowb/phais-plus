eeea0f161c892ad99e88e79249b37c4876129532
Victor Kauan <victorkauan.dev@gmail.com>
2024-03-27 02:17:41 -0300

    :sparkles: feat: Add get statuses module

--- server/requests/src/infra/routes.ts
+++ server/requests/src/infra/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { GetStatusesController } from "../modules/get-statuses/get-statuses.controller";
 import { GetRequestsController } from "../modules/get-requests/get-requests.controller";
 import { GetRequestController } from "../modules/get-request/get-request.controller";
 import { CreateRequestController } from "../modules/create-request/create-request.controller";
 
 const router = Router();
+
+router.get("/status", new GetStatusesController().handle);
 
 router.get("/", new GetRequestsController().handle);
 router.get("/:id", new GetRequestController().handle);
